42ca90f2cb4a50c063432738d2d6b07754a35ec7
Jason Albalah <jason.albalah@yahoo.com>
2018-12-15 17:30:10 +0000

    removed extra files

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     source_files = {'Transpile.py', 'main.py'}
     py_files = list(filter(None, ['' if (not x.endswith('.py') or x in source_files)
                                   else x for x in os.listdir(os.getcwd())]))
+
+
     for pyf in py_files:
         with open(pyf) as rf:
             tupl = Transpile(rf.readlines())
